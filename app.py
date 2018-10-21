c185027cdfb289cf09a19986a1035ba3ca4641d0
Ryan McManaman <McManaman.Ryan97@gmail.com>
2018-10-20 21:13:17 -0500

    POST stuff haha

--- app.py
+++ app.py
@@ -12,8 +12,39 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def home():
     return render_template('index.html')
 
+#Check if filetype is allowed because fuck trusting your users
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+#File Upload Logic
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return
+
+#Main application logic
 if __name__ == '__main__':
-	app.run()
-    os.system('waon -i happybirthday.wav -o birthday.mid')
-    if open('birthday.mid'):
-        print('yea')
+    app.secret_key = 'XD'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    app.debug = True
+    app.run()
+    # os.system('waon -i happybirthday.wav -o birthday.mid')
+    # if open('birthday.mid'):
+    #     print('yea')
